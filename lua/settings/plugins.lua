1ef1837e18ef359fcc6bb3fd1a7d7657011f1c49
nathan j <corigne@gmail.com>
2023-07-10 12:36:39 -0500

    Fixed incorrect barbar config for NvimTree

--- lua/settings/plugins.lua
+++ lua/settings/plugins.lua
@@ -46,7 +46,9 @@ require('lazy').setup({
     opts = {
       animation = true,
       insert_at_start = true,
-      NvimTree = true
+      sidebar_filetypes = {
+        NvimTree = true,
+      },
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
